a9b9ab588fea424e6a408cd90dacfb3e8fb4b895
shinusei <lyshagrishin@gmail.com>
2023-10-04 13:32:30 +0300

    part prac2

--- SIAOD2/main.cpp
+++ SIAOD2/main.cpp
@@ -3,42 +3,69 @@
 
 using namespace std;
 
-void printInFile(int n[], string name) {
+struct pacient{
+    int number;
+    int code;
+    char* sName=new char[20];
+};
+
+void formatPrint(pacient n){
+    cout << n.number << " " << n.code << " " << n.sName << endl;
+}
+
+void createFile(string fileName){
+    ofstream fileOut;
+    pacient n;
+    n.code=000;
+    n.number=000;
+    n.sName="Test";
+    fileOut.open(fileName, ios::out | ios::binary);
+    fileOut.write((char*)&n, sizeof( pacient));
+    n.code=1;
+    fileOut.write((char*)&n, sizeof( pacient));
+    n.code=2;
+    fileOut.write((char*)&n, sizeof( pacient));
+    fileOut.close();
+}
+
+void printInFile(pacient n, string name) {
     ofstream out(name, ios::out | ios::binary);
     if (!out) {
-        cout << "Не удается открыть файл";
+        cout << "Не удаётся открыть файл";
         return;
     }
-    out.write((char*)&n, sizeof n);
+    out.write((char*)&n, sizeof( pacient));
     out.close();
 }
 
 void printFromFile(string name) {
-    int n[3]{};
+    pacient n;
     ifstream in(name, ios::in | ios::binary);
     if (!in) {
         cout << "Не удается открыть файл";
         return;
     }
-    in.read((char*)&n, sizeof n);
-    for (int i = 0; i < 3; i++) {
-        cout << n[i] << endl;
+    while(!in.eof()) {
+        in.read((char*)&n, sizeof( pacient));
+        formatPrint(n);
     }
+
     in.close();
 }
 
-void fromBinTotext(string from, string to) {
+void fromBinToText(string from, string to) {
     ofstream fileOut;
     fileOut.open(to, ios::out | ios::trunc);
     ifstream fileIn;
     fileIn.open(from, ios::in | ios::binary);
-    int n[1024] = {};
-    fileIn.read((char*)&n, sizeof n);
-    fileOut.write((char*)&n, sizeof n);
+    pacient n;
+    fileIn.read((char*)&n, sizeof( pacient));
+    fileOut.write((char*)&n, sizeof( pacient));
     fileIn.close();
     fileOut.close();
 }
 
 int main() {
-
+    createFile("A.bin");
+    printFromFile("A.bin");
 }
